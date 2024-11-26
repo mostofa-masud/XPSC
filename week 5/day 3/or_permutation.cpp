c33e92e3541f8881ad74d761493bfff28cb96bff
Mostofa Masud <mdmostofamasudnoman@gmail.com>
2024-11-26 23:10:00 +0600

    week 5 day 3 uploaded

---
+++ week 5/day 3/or_permutation.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define int long long
+#define endl "\n"
+using namespace std;
+int32_t main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        for (int i = n; i > 0; i--)
+            cout << i << " ";
+        cout << endl;
+    }
+    return 0;
+}
